d32bd45cbf20725bb10bce41e3621af508b91e72
Tanyboy3333 <tanveer.24mas10044@vitbhopal.ac.in>
2026-05-02 16:34:43 +0530

    complete

--- template.py
+++ template.py
@@ -4,6 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
+
 project_name = "cnnClassifier"
 
 list_of_files = [
@@ -24,21 +25,25 @@ list_of_files = [
     "research/trials.ipynb",
     "templates/index.html"
 
+
 ]
+
 
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir != "":
+
+    if filedir !="":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory; {filedir} for the file : {filename}")
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
 
+
     else:
-        logging.info(f"{filename} already exists")
+        logging.info(f"{filename} is already exists")
